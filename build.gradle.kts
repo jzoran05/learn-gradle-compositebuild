101f57a5fd4611a31491f6f129ac4634dbd384a8
zoran <zoranjov@gmail.com>
2019-08-27 23:37:36 +0200

    baseline

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    kotlin("jvm") version "1.3.50"
+    idea
+    //application
+}
+
+group = "com.learn.gradle.compositebuild"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("com.learn.gradle.compositeBuild:project01:1.0-SNAPSHOT")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/main/kotlin/MyApp.kt
@@ -0,0 +1,9 @@
+
+
+fun main() {
+
+    val project01Util = Utility01()
+    val result = project01Util.function01(5)
+    println("MyApp::Kotlin Hello World!")
+    println("Result is $result")
+}
